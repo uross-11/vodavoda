72d565e906fdd1f9603a12d2a412ccbf73fceb21
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-05-13 15:02:45 +0200

    Fix scroll bug.

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ const hamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.nav');
 const overlay = document.querySelector('.overlay');
 const overlayMenu = document.querySelector('.overlay-menu');
+const html = document.querySelector('html');
 
 hamburger.addEventListener('click', () => {
 
   if(header.classList.contains('open')) { // Close hamburger menu
     header.classList.remove('open');
+    html.classList.remove('open');
 
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
@@ -22,6 +24,7 @@ hamburger.addEventListener('click', () => {
     overlayMenu.classList.add('fade-out');
   } else { // Open hamburger menu
     header.classList.add('open');
+    html.classList.add('open');
 
     overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
